fb18da1e11cf6480349bd83c4db9d7ee01900826
Devin <devinborchard@gmail.com>
2022-07-15 10:57:49 -0400

    added react routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import Login from './components/Login';
+import Home from './components/Home';
 import './App.css';
 import { Counter } from './components/Counter';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
+    <Router>
+    <div>
+        <Routes>
+            <Route path="/login" element={<Login />}/>
+        </Routes>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+        </Routes>
     </div>
+</Router>
   );
 }
 
